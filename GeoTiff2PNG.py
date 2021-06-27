7ee53ccdb15640b76935998a071a0f6d7a337411
sayeda1 <71078969+sayeda1@users.noreply.github.com>
2021-06-27 20:18:10 +0400

    Add files via upload

--- GeoTiff2PNG.py
+++ GeoTiff2PNG.py
@@ -1,30 +1,15 @@
 from osgeo import gdal
-# import os
-# from PIL import Image
-
-# root_dir = 'C:/Users/Amaan-PC/Desktop/DatumCon/'
-# for filename in os.listdir(root_dir):
-#     infile = os.path.join(root_dir, filename)
-#     print("file : " + infile)
-#     if infile[-3:] == "tif" or infile[-3:] == "bmp" :
-#        # print "is tif or bmp"
-#        outfile = infile[:-3] + "jpeg"
-#        im = Image.open(infile)
-#        print("new filename : " + outfile)
-#        out = im.convert("RGB")
-#        out.save(outfile, "JPEG", quality=100)
 
 options_list = [
     '-ot Byte',
     '-of JPEG',
-    '-b 1',
     '-scale'
 ]           
 
 options_string = " ".join(options_list)
     
 gdal.Translate(
-    'save_image_path.jpg',
-    'C:\Users\Amaan-PC\Desktop\DatumCon\s63_export.tif',
+    'C:/Users/Amaan-PC/Desktop/DatumCon/TEST8.jpg',
+    'C:/Users/Amaan-PC/Desktop/DatumCon/s63_export.tif',
     options=options_string
 )
